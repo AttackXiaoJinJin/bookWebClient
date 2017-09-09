a519e77183e4ccd31aa513419b9ed0723b4fbe94
AttackXiaoJinJin <wwwcenjin1314@gmail.com>
2017-09-09 22:49:13 +0800

    个人空间

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonalCenterComponent } from './personal-center/personal-center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalCenterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/personal-center/personal-center.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-center',
+  templateUrl: './personal-center.component.html',
+  styleUrls: ['./personal-center.component.css']
+})
+export class PersonalCenterComponent implements OnInit {
+  tabs=['喜欢书籍','订单','收货地址','关注话题','收藏文章','消息'];
+  tab_index=0;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
