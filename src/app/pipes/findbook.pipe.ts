528bfafdc4f6ed56eca0d9fe09337aa0aca75c4d
StupidFoxY <623783696@qq.com>
2017-10-09 16:56:01 +0800

    添加find的相关注释

--- src/app/pipes/findbook.pipe.ts
+++ src/app/pipes/findbook.pipe.ts
@@ -9,16 +9,17 @@ export class FindbookPipe implements PipeTransform {
   ) { }
 
   transform(books: any, args?: any): any {
+    //args传过来的参数数组可能是一个也可能是两个
     if (books) {
-      if (args[0] && args[1]) {
+      if (args[0] && args[1]) {//两个都存在时
         var new_books = books.filter(function (book, index) {
           if (book.book_tag.indexOf(args[0]) != -1 || book.book_tag.indexOf(args[1]) != -1) {
             return book;
           }
         });
-        this.glo.bookcount = new_books.length;
+        this.glo.bookcount = new_books.length;//将分类筛选还的数组长度存到全局变量中
         return new_books;
-      } else if (args[0] && !args[1]) {
+      } else if (args[0] && !args[1]) {//一个存在时
         var new_books = books.filter(function (book, index) {
           if (book.book_tag.indexOf(args[0]) != -1) {
             return book;
@@ -26,7 +27,7 @@ export class FindbookPipe implements PipeTransform {
         });
         this.glo.bookcount = new_books.length;
         return new_books;
-      } else {
+      } else {//不存在时
         this.glo.bookcount = books.length;
         return books;
       }
